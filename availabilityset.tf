efe34355d4fb1935c18ca64bd0f2481fc625467f
ramansat87 <36228424+ramansat87@users.noreply.github.com>
2019-06-22 20:07:09 +0530

    Update availabilityset.tf

--- availabilityset.tf
+++ availabilityset.tf
@@ -1,3 +1,14 @@
+#create a resource group
+resource "azurerm_resource_group" "rg"{
+		name 		= "Test_Zone"
+		location 	= "southindia"
+	tags = {
+	Environment = "Development"
+	Users	    = "Test_Users"
+		}	
+}
+
+
 #Create a availability set
 resource "azurerm_availability_set" "as" {
 		name 	            = "HA"
