7053ff4c6ee721287cc789737b89385543c6da9c
ramansat87 <36228424+ramansat87@users.noreply.github.com>
2019-06-22 21:44:58 +0530

    Update virtualnetwork.tf

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -38,6 +38,6 @@ resource "azurerm_subnet" "snet" {
 		name		= "snet1"
 resource_group_name	= "${azurerm_resource_group.rg.name}"
 virtual_network_name= "${azurerm_virtual_network.vnet.name}"
-address_prefix 		= "10.1.0.0/24"
+address_prefix 		= "10.0.1.0/24"
 }		
 	
